b0bafeb8f331eaf97a6e27e6546f2b652e6e9fce
Chloe Peng <chloepeng331@icloud.com>
2022-01-16 14:01:32 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here! 
+//no longer has DOM node 'main#main'
+const main = document.getElementById("main");
+main.remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory':
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<p>Chloe is the champion</p>";
+document.body.append(newHeader);
+
