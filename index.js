429a332b4211dc4c273f88d2b3f70a958fb579f3
Chloe Peng <chloepeng331@icloud.com>
2022-02-07 20:04:47 -0500

    remember to pass the new variable not the class name

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// Write your code here! 
-//no longer has DOM node 'main#main'
-const main = document.getElementById("main");
+const main = document.querySelector('main');
 main.remove();
 
-//has a 'newHeader' variable that points to node 'h1#victory':
-const newHeader = document.createElement("h1");
-newHeader.id = "victory";
-newHeader.innerHTML = "<p>Chloe is the champion</p>";
-document.body.append(newHeader);
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Chloe is the champion";
+body.append(newHeader);
 
